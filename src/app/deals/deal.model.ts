3d320e1e1f22159ef14fc95489bffcddd998836a
Jiapei Liang <liang98@purdue.edu>
2017-06-29 20:24:23 -0400

    Updated Deal model

--- src/app/deals/deal.model.ts
+++ src/app/deals/deal.model.ts
@@ -5,31 +5,53 @@ import { User } from 'app/shared/user.model';
 export class Deal {
 
   constructor(
+    public allowPickup: boolean,
+    public commentsCount: number,
+    public createdAt: number,
     public dealItems: DealItem[],
     public dealPickupLocations: DealPickupLocation[],
     public description: string,
     public endTime: number,
     public id: string,
+    public imagePaths: string[],
+    public isPointsDeal: boolean,
+    public isResourceDeal: boolean,
+    public isWholesale: boolean,
+    public language: string,
+    public liked: boolean,
     public likesCount: number,
     public official: boolean,
     public orderCount: number,
     public owner: User,
+    public paymentTime: number,
     public pickupTime: number,
+    public promoted: boolean,
     public startTime: number,
     public status: string,
     public title: string,
     public viewCount: number
   ) {
+    this.allowPickup = allowPickup;
+    this.commentsCount = commentsCount;
+    this.createdAt = createdAt;
     this.dealItems = dealItems;
     this.dealPickupLocations = dealPickupLocations;
     this.description = description;
     this.endTime = endTime;
     this.id = id;
+    this.imagePaths = imagePaths;
+    this.isPointsDeal = isPointsDeal;
+    this.isResourceDeal = isResourceDeal;
+    this.isWholesale = isWholesale;
+    this.language = language;
+    this.liked = liked;
     this.likesCount = likesCount;
     this.official = official;
     this.orderCount = orderCount;
     this.owner = owner;
+    this.paymentTime = paymentTime;
     this.pickupTime = pickupTime;
+    this.promoted = promoted;
     this.startTime = startTime;
     this.status = status;
     this.title = title;
