101bc013390c22b271c09e93c0a06b0769801a14
Jiapei Liang <liang98@purdue.edu>
2017-07-02 10:32:02 -0400

    Added deal-item component into deal-detail

--- src/app/deals/deal-list/deal/deal-item/deal-item.component.ts
+++ src/app/deals/deal-list/deal/deal-item/deal-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { DealItem } from '../../../deal-item.model';
 
 @Component({
   selector: 'app-deal-item',
@@ -6,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deal-item.component.css']
 })
 export class DealItemComponent implements OnInit {
+  
+  @Input() dealItem: DealItem;
+  
+  amount:number = 0;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onAdd() {
+    if (this.dealItem.available) {
+      if (this.amount < this.dealItem.available) {
+        this.amount += 1;
+      }
+    } else {
+      this.amount += 1;
+    }
+    
+
+
+  }
+
+  onMinus() {
+    if (this.amount > 0) {
+      this.amount -= 1;
+    }
   }
 
 }
